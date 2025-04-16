439bcde7bb5fee92ea394001c4ffefc6dc8b2cc8
Rakesh1471 <rakeshcivilengg226@gmail.com>
2025-04-16 19:55:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.medium"
 key_name = "rk"
-vpc_security_group_ids = ["sg-06de78b1710ca79b6"]
+vpc_security_group_ids = ["sg-03cda2e18fc1b5067"]
 tags = {
 Name = var.instance_names[count.index]
 }
